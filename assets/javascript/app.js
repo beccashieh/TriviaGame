d72b278a53e0867f7646dfb99af180f2c66d27c4
BShieh <rebeccasshieh@gmail.com>
2019-03-27 17:57:30 -0400

    wip: functions updates and some styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,53 +2,76 @@
 $(document).ready(function() {
 
 //global variables:
-var questionArr = [
+var questionObj = [
     {
-        question: "How many fingertips did Stannos chop off of Davos' hand?",
-        choices: {"One", "Two", "Three", "Four"},
+        question: "How many fingertips did Stannis chop off of Davos' hand?",
+        choices: ["One", "Two", "Three", "Four"],
         correctAnswer: "Four"
     },
     {
         question: "Who is king of Westeros when the series began?",
-        choices: {"Eddard Stark", "Aerys Targaryan", "Robert Baratheon", "Theon Greyjoy"},
+        choices: ["Eddard Stark", "Aerys Targaryan", "Robert Baratheon", "Theon Greyjoy"],
         correctAnswer: "Robert Baratheon"
     },
     {
         question: "At the end of his training, what must an unsullied kill to prove he has no weakness?",
-        choices: {"A baby calf", "A member of his training group", "A newborn child", "An elderly slave"},
+        choices: ["A baby calf", "A member of his training group", "A newborn child", "An elderly slave"],
         correctAnswer: "A newborn child"
     },
     {
         question: "What house is Catelyn Stark from?",
-        choices: {"Baratheon", "Tyrell", "Targaryan", "Tully"},
+        choices: ["Baratheon", "Tyrell", "Targaryan", "Tully"],
         correctAnswer: "Tully"
     },
     {
         question: "What is the name of the person who chopped off Jaime's right hand?",
-        choices: {"Locke", "Tyrion Lannister", "Breanne of Tarth", "The Mountain"},
+        choices: ["Locke", "Tyrion Lannister", "Breanne of Tarth", "The Mountain"],
         correctAnswer: "Locke"
     },
 ];
 var correct = 0;
 var inCorrect= 0;
 
+//Populates each new question from the questionObj into the ".question" class html.
+function quiz(){
+    for (var i = 0; i < questionObj.length; i++){
+        $(".question").html(questionObj[i].question);
+        for (var j = 0; j < questionObj.length; j++){
+            $("#test1").html(questionObj[i].choices[0]);
+            $("#test2").text(questionObj[i].choices[1]);
+            $("#test3").text(questionObj[i].choices[2]);
+            $("#test4").text(questionObj[i].choices[3]);
+        }
+    }
+
+    
+    //function check(){
+        //$("#after-submit").css("visibility", "visible");
+        //$("#number-correct").html("You got " + correct + " correct and " + inCorrect + "incorrect!")
+    }
+quiz();
+});
+
+
+//Populates each corresponding choices array from the questionObj into the ".answer" class radio buttons.
+
+
 //Sets up audio element
-var audioElement = document.createElement("audio");
-audioElement.setAttribute("src", "Assets/gameOfThrones.mp3");
+//var audioElement = document.createElement("audio");
+//audioElement.setAttribute("src", "./assets/gameOfThrones.mp3");
 // Theme Button-- plays music on click.
-    $(".theme-button").on("click", function() {
-        audioElement.play();
-      });
+    //$(".theme-button").on("click", function() {
+        //audioElement.play();
+      //});
 
 // Pause Button-- pauses music on click.
-    $(".pause-button").on("click", function() {
-        audioElement.pause();
-      });
+    //$(".pause-button").on("click", function() {
+        //audioElement.pause();
+      //});
+
 //questions will show with 4 possible choices. 
-function check(){
-    $("#after-submit").css("visibility", "visible");
-    $("#number-correct").html("You got " + correct + " correct and " + inCorrect + "incorrect!")
-}
+
+
     //timer will alot 30 seconds for each question. 
         //if no answer selected, will say "Time's up!", show correct answer and picture.
             //3 second timer for new question.
